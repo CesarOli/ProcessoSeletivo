058dbd847f3e70e7738149feb0c64f3e086d8f97
CesarOli <cesaraleoliveira@gmail.com>
2024-06-08 00:51:00 -0300

    Inicialização do Projeto 'Processo Seletivo'.

---
+++ ProcessoSeletivo.java
@@ -0,0 +1,8 @@
+package candidatura;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ProcessoSeletivo {
+    public static void main(String[] args) {
+        System.out.println("O Processo Seletivo.");
+    }
+}
