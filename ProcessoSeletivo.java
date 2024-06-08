33d42a8a6442a82457e50902c04d9d06a89302a9
CesarOli <cesaraleoliveira@gmail.com>
2024-06-08 01:18:09 -0300

    Criação da classe 'Analisar Candidadtos.'

--- ProcessoSeletivo.java
+++ ProcessoSeletivo.java
@@ -1,8 +1,21 @@
 package candidatura;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
         System.out.println("O Processo Seletivo.");
+        analisarCandidatos(1900.0);
+        analisarCandidatos(2200.0);
+        analisarCandidatos(2000.0);
+    }
+
+    static void analisarCandidatos(double salarioPretendido) {
+        double salarioBase = 2000.0;
+        if (salarioBase > salarioPretendido) {
+            System.out.println("LIGAR PARA O CANDIDATO.");
+        } else if(salarioBase == salarioPretendido) {
+            System.out.println("LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA.");
+        } else {
+            System.out.println("AGUARDANDO O RESULTADO DOS DEMAIS CANDIDATOS");
+        }
     }
 }
